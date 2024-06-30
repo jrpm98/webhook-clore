b884e54c3a9a313a238e26f3be2e2ba7a67a1c65
jrpm98 <84054982+jrpm98@users.noreply.github.com>
2024-06-30 17:48:18 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export IP=$(curl -s https://api.ipify.org)
 export WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b93
 WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b9375e46"
-curl -X POST -d IP address: $IP "$WEBHOOK_URL"
+# Retrieve the public IP address
+IP=$(curl -s https://api.ipify.org)
+
+# Send the IP address to your webhook
+curl -X POST -d "IP address: $IP" https://webhook.site/eb91a994-348f-43eb-847c-7849b9375e46
