4808b7626cc7f82c698ad1725401d8dd1d42d726
jrpm98 <84054982+jrpm98@users.noreply.github.com>
2024-06-30 17:53:52 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export IP=$(curl -s https://api.ipify.org)
-export WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b93
-WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b9375e46"
+
 # Retrieve the public IP address
 IP=$(curl -s https://api.ipify.org)
 
