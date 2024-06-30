acee02155e38a71985ef00b28d34744b76c5a802
jrpm98 <84054982+jrpm98@users.noreply.github.com>
2024-06-30 17:44:47 -0500

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b93
+WEBHOOK_URL="https://webhook.site/eb91a994-348f-43eb-847c-7849b9375e46"
+curl -X POST -d IP address: $IP "$WEBHOOK_URL"
